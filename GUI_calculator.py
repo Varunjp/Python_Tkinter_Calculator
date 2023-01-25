9ff164d71ede6f078026cecf754f91f48ef30704
Varun jp <varunjp@gmail.com>
2023-01-25 14:04:53 +0530

    Inital commit with basic window

---
+++ GUI_calculator.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# window section ---------------------------
+
+Window = Tk()
+Window.title("Calculator")
+Window.geometry("400x500")
+Window.configure(bg="#2e76e8")
+
+# window section ---------------------------
+
+
+Window.mainloop()
